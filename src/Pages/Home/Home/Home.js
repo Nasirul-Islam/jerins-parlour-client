b1e66fc2aa4a925b5fdd0fdd8d744ebf11867e34
Nasirul-Islam <nasirulislam866@gmail.com>
2021-11-18 15:54:39 +0600

    initial setup

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Professional from '../Professional/Professional';
+import Project from '../Project/Project';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Professional></Professional>
+            <Project></Project>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/Professional/Professional.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Professional = () => {
+    return (
+        <div>
+            <h2>this is Professional</h2>
+        </div>
+    );
+};
+
+export default Professional;
